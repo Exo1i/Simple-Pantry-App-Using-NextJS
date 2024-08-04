6f61042965244ec45850c0e8ad1f5cf399405031
Exo <121557650+Exo1i@users.noreply.github.com>
2024-08-04 23:55:12 +0300

    visual changes

--- app/componets/columns.jsx
+++ app/componets/columns.jsx
@@ -2,8 +2,8 @@ const columns = [
   { field: "id", headerName: "ID", width: 100 },
   { field: "eanCode", headerName: "EAN", width: 200 },
   { field: "productName", headerName: "Product Name", width: 150 },
-  { field: "nutritionFacts", headerName: "Nutrition Facts", width: 200 },
-  { field: "brand", headerName: "Brand", width: 200 },
+  { field: "nutritionFacts", headerName: "Nutrition Facts", width: 100 },
+  { field: "brand", headerName: "Brand", width: 150 },
   { field: "quantity", headerName: "Quantity", width: 200 },
 ];
 
